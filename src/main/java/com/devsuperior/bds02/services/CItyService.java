877c9349f14dec841f8ee9afb045143f48b10b49
Guilherme Campos <guiicampos31@gmail.com>
2025-12-14 22:36:34 -0300

    test insertShouldInsertResource passed

--- src/main/java/com/devsuperior/bds02/services/CItyService.java
+++ src/main/java/com/devsuperior/bds02/services/CItyService.java
@@ -23,4 +23,14 @@ public class CItyService {
         List<City> list = repository.findAll(Sort.by("name"));
         return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
     }
+
+    @Transactional
+    public CityDTO insert(CityDTO dto) {
+
+        City entity = new City();
+        entity.setName(dto.getName());
+
+        entity = repository.save(entity);
+        return new CityDTO(entity);
+    }
 }
